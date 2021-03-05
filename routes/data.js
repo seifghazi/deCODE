4b28220061d773bf9a72ca9af3d4c16968e82dea
seifghazi <seifghazi@gmail.com>
2021-03-05 20:52:57 +0300

    Added frequency counts to question endpoints

--- routes/data.js
+++ routes/data.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const mongo = require('../db/mongo')
@@ -12,18 +13,62 @@ router.get('/', (req, res) => {
 /**
  * Returns questions and response frequency
  */
-router.get('/quiz', async (req, res) => {
+router.get('/questions', async (req, res) => {
+    // fetch question from db
+    const questions = mongo.client.db('test').collection('quizQuestions');
+    let result = await questions.find().toArray();
+
+    // calculate responseCount along with percentage for each question and its choices
+    let formattedQuesResponse = {};
+    result.forEach((question) => {
+        let totalResponseCount = question.responseFrequency.reduce((a, b) => a + b, 0);
+
+        let responses = {};
+        question.responseFrequency.forEach((responseFreq, idx) => {
+            responses[idx] = {
+                numTimesAnswered: responseFreq,
+                percentage: Math.round((responseFreq / totalResponseCount) * 100, 1)
+            }
+        })
+
+        formattedQuesResponse[`q${question.questionID}`] = responses;
+    })
+
+    res.send(JSON.stringify(formattedQuesResponse))
+})
+
+/**
+ * Return stats for specific question
+ */
+router.get('/questions/:questionID', async (req, res) => {
+    const questionID = req.params.questionID
+
     // fetch question from db
     const questions = mongo.client.db('test').collection('quizQuestions');
     let question = await questions.findOne({ questionID });
+    let responseFrequency = question.responseFrequency
 
-    res.send(JSON.stringify(question))
+    // calculate responseCount along with percentage for each choice
+    let formattedQuesResponse = {};
+    let totalResponseCount = responseFrequency.reduce((a, b) => a + b, 0);
+
+    let responses = {};
+    question.responseFrequency.forEach((responseFreq, idx) => {
+        responses[idx] = {
+            numTimesAnswered: responseFreq,
+            percentage: Math.round((responseFreq / totalResponseCount) * 100, 1)
+        }
+    })
+
+    formattedQuesResponse[`q${question.questionID}`] = responses;
+
+    res.send(formattedQuesResponse)
 })
 
 /**
  * Receives response for question 'qID' and updates db with count
  */
-router.post('/quiz/:questionID', async (req, res) => {
+router.post('/questions/:questionID', async (req, res) => {
     const questionID = req.params.questionID;
     const selectedAnswer = req.body.response;
 
